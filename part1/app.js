165ebb14b3cc07fc7c21482ca733ca3117cc9489
jeffreykeough <13545francisjoseph>
2025-06-20 11:09:32 +0000

    [automatic commit] 2025-06-20 11:09:31 UTC 203.34.165.23

--- part1/app.js
+++ part1/app.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Database connection
 let db;
 
